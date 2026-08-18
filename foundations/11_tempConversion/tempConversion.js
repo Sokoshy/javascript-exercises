dcb7edebeece9dce334eabb6c0e90e3890ae1bb6
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-08-18 19:57:14 +0200

    completed foundations/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahren) {
+  return Math.round((tempFahren - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+   let fahrenheit = tempCels * 1.8 + 32;
+  return Math.round(((tempCels * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
