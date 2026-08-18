35b959a6922fe221380eeeb3db32ca3b018d3e12
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-08-18 18:19:06 +0200

    completed foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+  if (!Number.isInteger(first) || !Number.isInteger(last) || first < 0 || last < 0) return "ERROR";
+  if (first > last){
+    let swap = first;
+    first = last;
+    last = swap;
+  }
 
+  let result = 0;
+  for(let i = first; i <= last; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
