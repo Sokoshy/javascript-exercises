4375ef50b610bbd2b79d3d5fe0f6b30e0c18b58f
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-08-18 19:04:18 +0200

    completed foundations/10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
+const leapYears = function(years) {
+  const isBisextile =
+  (years % 4 === 0 && years % 100 === 0 && years % 400 === 0) ||
+  (years % 4 === 0 && years % 100 !== 0);
 
-};
+return isBisextile;};
 
 // Do not edit below this line
 module.exports = leapYears;
