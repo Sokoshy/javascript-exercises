f8b2e8a0a4ae5fef5c97257332a8f4304bccf98c
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-08-26 17:13:15 +0200

    completed foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  number = Number.parseInt(number);
 
+  if (number < 0 || Number.isNaN(number)) {
+    return "OOPS";
+  }
+
+  if (number === 0) {
+    return number;
+  }
+
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for (let i = 2; i <= number; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+
+  return firstPrev;
+
+  //recursive method
+  //if (number <= 1) {
+  //  return number;
+  //}
+
+  //return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
