3e5e42b146b6fbab08519539656e85d107807505
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-08-27 12:27:10 +0200

    completed foudations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestPerson = people[0];
+  return people.reduce((oldest, current) => {
+    if (getAge(oldest) < getAge(current)) {
+      return current;
+    }
+    return oldest;
+  }, oldestPerson)
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+function getAge(person) {
+  let yearToCalculateAge = person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
+  return yearToCalculateAge - person.yearOfBirth;
+}
